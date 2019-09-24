14eede1be74a5bd4f60cae31fecbf9a019c0e471
peteruche21 <anyaogupeter601@gmail.com>
2019-09-24 13:09:06 +0100

    added form and changed logo no major coding

---
+++ js/Untitled-1.js
@@ -0,0 +1,46 @@
+function isEven(x) {
+    if (x % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+isEven(4);
+
+function isEven(x) {
+    if (x % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+isEven(21);
+
+function isEven(x) {
+    if (x % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+isEven(68);
+function isEven(x) {
+    if (x % 2 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+    }
+    isEven(333);
+
+function isEven(x) {
+    var factorial = 1;
+ for( var count = 2 ; count <= x; count++ ) {
+     factorial = factorial * count;
+ }
+ return factorial;
+}
+
+
+
